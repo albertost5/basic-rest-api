1b40c9e3b22d2807de170c3d3766e4a2fce3d74b
albertost5 <sancheztorresalberto@gmail.com>
2022-04-21 00:01:39 +0200

    Added bcrypt package to hash pws

--- src/api/routes/controllers/user.controller.js
+++ src/api/routes/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../../models/user');
 const customErrorResponse = require('../../../utils/error.util');
+const bcrypt = require('bcrypt');
 
 
 class UserController {
@@ -13,21 +14,35 @@ class UserController {
     }
 
     __createUser = async (req, res) => {
+
+        const { name, email, pw } = req.body
+
         
-        const user = new User( req.body );
-
-        if( Object.keys(req.body).length > 0 ) {
-
+        if( name && email && pw ) {
+            
+            const saltRounds = 10;
+            
             try {
+
+                const user = new User({
+                    name: name,
+                    email: email,
+                    password: bcrypt.hashSync(pw, saltRounds)
+                });
+            
                 await user.save();
+
+                res.status(201).json({
+                    message: `The user with email ${ user.email } was created successfully!`
+                });
             } catch (error) {
                 return res.status(400).json( customErrorResponse('40001', 'BAD_REQUEST', 'There was a problem saving the user.') );
             }
 
-            res.json( user );
         } else {
-            res.status(400).json( customErrorResponse('40000', 'BAD_REQUEST', 'Bad request, incorrect data.') );
+            res.status(400).json( customErrorResponse('40000', 'BAD_REQUEST', 'Incorrect data.') );
         }
+        
     }
 }
 
